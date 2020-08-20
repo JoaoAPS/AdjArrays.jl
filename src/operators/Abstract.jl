f6efbdb204d56838cdd8860d996459029be2c08d
João Paludo <jas.paludo@gmail.com>
2020-08-20 14:00:07 -0300

    Bugfix

--- src/operators/Abstract.jl
+++ src/operators/Abstract.jl
@@ -275,5 +275,5 @@ function equivalentLatticeNetwork(
 		
 	newnet = CustomNetwork(SparseArrays.dropzeros(mat), directed=true)
 	
-	return clusteringCoefficient(newnet) > clusteringCoefficient(network) ? network : newnet
+	return clusteringcoefficient(newnet) > clusteringcoefficient(network) ? network : newnet
 end
